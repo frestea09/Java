6bf0e3705b74703f54391b3fe2124d2970093ede
ilman teguh prasetya <ilmanfrasetya@gmail.com>
2019-03-04 19:45:07 +0700

    First Commit Hello World

---
+++ src/helloWorld.java
@@ -0,0 +1,7 @@
+//helloWorld.java
+public class helloWorld{
+    public static void main(String[] args){
+        System.out.print("hello World");
+        System.out.print("ilman ganteng");
+    }
+}
